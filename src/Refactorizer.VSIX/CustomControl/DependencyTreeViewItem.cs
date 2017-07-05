0945eb60d245fdb65342f09662b763aeae69e2d5
Frank Kubis <frank.kubis@saxsys.de>
2017-07-05 11:55:28 +0200

    #1 #2 Fixing position of adorner lines and handle different states of visibility. Remove if expanded or not visible

--- src/Refactorizer.VSIX/CustomControl/DependencyTreeViewItem.cs
+++ src/Refactorizer.VSIX/CustomControl/DependencyTreeViewItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
+using Refactorizer.VSIX.Models;
 using Refactorizer.VSIX.ViewModels;
 
 namespace Refactorizer.VSIX.CustomControl
@@ -11,7 +12,7 @@ namespace Refactorizer.VSIX.CustomControl
     {
         private readonly DependencyTreeView _host;
 
-        private readonly Dictionary<Guid, BezierCurveAdorner> _drawedLines = new Dictionary<Guid, BezierCurveAdorner>();
+        private Dictionary<Guid, BezierCurveAdorner> _drawedLines = new Dictionary<Guid, BezierCurveAdorner>();
 
         public DependencyTreeViewItem(DependencyTreeView host)
         {
@@ -50,45 +51,83 @@ namespace Refactorizer.VSIX.CustomControl
             var viewModel = DataContext as DependencyTreeViewItemViewModel;
 
             if (viewModel != null)
-                foreach (var reference in viewModel.RelatedModel.References)
+                if (IsExpanded || !IsVisible)
                 {
-                    var referenceTreeViewItem = _host.FindReferencedItemOrParent(reference);
-                    if (referenceTreeViewItem == null)
-                        continue;
-
-                    var from = new Point(0, 0);
-                    var controlOne = new Point(0, 0);
-                    var controlTwo = new Point(0, 0);
-                    var to = referenceTreeViewItem.TranslatePoint(new Point(0, 0), this);
-
+                    DeleteLines();
+                }
+                else
+                {
                     var xOffset = 30;
-                    var yOffset = 10;
+                    var yOffset = 12;
+                    var relatedModel = viewModel.RelatedModel;
 
-                    from.Y += yOffset;
-                    to.Y = from.Y > to.Y ? to.Y + yOffset : to.Y - yOffset;
-
-                    controlOne.X = from.X - xOffset;
-                    controlOne.Y = from.Y;
-
-                    controlTwo.X = to.X - xOffset;
-                    controlTwo.Y = to.Y;
-
-                    if (_drawedLines.ContainsKey(reference.Id))
+                    foreach (var reference in relatedModel.References)
                     {
-                        var adorner = _drawedLines[reference.Id];
-                        adorner.From = from;
-                        adorner.ControlTwo = controlOne;
-                        adorner.ControlTwo = controlTwo;
-                        adorner.To = to;
-                        adorner.UpdateLayout();
-                    } else {
-                        var adorner = new BezierCurveAdorner(this, from, controlOne, controlTwo, to);
-                        OutLines.Add(adorner);
+                        var referenceTreeViewItem = _host.FindReferencedItemOrParent(reference);
+                        if (referenceTreeViewItem == null)
+                            continue;
 
-                        _host.OutReferencesAdornerLayer.Add(adorner);
-                        _drawedLines.Add(reference.Id, adorner);
+                        var from = new Point(0, 0);
+                        var controlOne = new Point(0, 0);
+                        var controlTwo = new Point(0, 0);
+                        var to = referenceTreeViewItem.TranslatePoint(new Point(0, 0), this);
+
+                        from.Y += yOffset;
+                        to.Y = from.Y > to.Y ? to.Y + yOffset : to.Y - yOffset;
+
+                        var fromXOffset = xOffset;
+                        var toXOffset = xOffset;
+
+                        if (relatedModel.Parent != null && !relatedModel.Parent.Id.Equals(reference.Parent.Id))
+                        {
+                            fromXOffset = xOffset * (relatedModel is Class ? 3 : relatedModel is Namespace ? 2 : 1);
+                            toXOffset = xOffset * (reference is Class ? 3 : reference is Namespace ? 2 : 1);
+                        }
+
+                        controlOne.X = from.X - fromXOffset;
+                        controlOne.Y = from.Y;
+
+                        controlTwo.X = to.X - toXOffset;
+                        controlTwo.Y = to.Y;
+
+                        if (_drawedLines.ContainsKey(reference.Id))
+                        {
+                            var adorner = _drawedLines[reference.Id];
+                            // Only update if some some point has changed
+                            if (adorner.From != from ||
+                                adorner.ControlOne != controlOne ||
+                                adorner.ControlTwo != controlTwo ||
+                                adorner.To != to)
+                            {
+                                adorner.From = from;
+                                adorner.ControlOne = controlOne;
+                                adorner.ControlTwo = controlTwo;
+                                adorner.To = to;
+                                adorner.UpdateLayout();
+                            }
+                        }
+                        else
+                        {
+                            var adorner = new BezierCurveAdorner(this, from, controlOne, controlTwo, to);
+                            // Make adorner not clickable, otherwise we could get in trouble open or closing a tree item
+                            adorner.IsHitTestVisible = false;
+                            OutLines.Add(adorner);
+
+                            _host.OutReferencesAdornerLayer.Add(adorner);
+                            _drawedLines.Add(reference.Id, adorner);
+                        }
                     }
                 }
+        }
+
+        private void DeleteLines()
+        {
+            foreach (var keyValuePair in _drawedLines)
+                _host.OutReferencesAdornerLayer.Remove(keyValuePair.Value);
+
+            _host.OutReferencesAdornerLayer.UpdateLayout();
+
+            _drawedLines = new Dictionary<Guid, BezierCurveAdorner>();
         }
     }
 }
--- src/Refactorizer.VSIX/Models/IModel.cs
+++ src/Refactorizer.VSIX/Models/IModel.cs
@@ -6,8 +6,11 @@ namespace Refactorizer.VSIX.Models
     public interface IModel
     {
         Guid Id { get; }
+
         string Name { get; set; }
+
         IModel Parent { get; set; }
+
         ICollection<IModel> References { get; set; }
     }
 }
